48af9d2759395779b27a51f2c9b76ce208e5b226
ソラノアナ Soranoana <Inanis_foramen@yahoo.co.jp>
2021-12-19 14:22:45 +0900

    UpTo Unity 2021.2.7f1

--- Assets/EarthRotation.cs
+++ Assets/EarthRotation.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 //地球の自転
-public class EarthRotation : MonoBehaviour {
+public class EarthRotation : MonoBehaviour
+{
 
     //時間管理プログラムのインスタンス化
     [SerializeField]
@@ -28,33 +29,37 @@ public class EarthRotation : MonoBehaviour {
     public bool doTimeLead;
     //現在の時刻
     [SerializeField]
-    private int month,day,hour, minutes, seconds,miliSeconds;
-    
+    private int month, day, hour, minutes, seconds, miliSeconds;
+
     //以下ローカル変数
     //回転角
     private float rotateRad;
     //1フレーム前の時間
     int timeWas;
 
-    void Start() {
+    void Start()
+    {
         worldClock = GameObject.Find("sunCentor").GetComponent<TimeAdministration>();
         clockUpdate();
     }
 
-    void Update() {
+    void Update()
+    {
         clockUpdate();
-        rotateRad = (( hour * 60 * 60 + minutes * 60 + seconds )-timeWas) / (float)( 24 * 60 * 60 );
+        rotateRad = ((hour * 60 * 60 + minutes * 60 + seconds) - timeWas) / (float)(24 * 60 * 60);
         //Debug.Log(rotateRad);
         //回転角の決定
-        transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, transform.localEulerAngles.y+rotateRad*360, transform.localEulerAngles.z);
+        transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, transform.localEulerAngles.y + rotateRad * 360, transform.localEulerAngles.z);
         //        transform.Rotate(new Vector3(0, rotateRad*10, 0), Space.Self);
         timeWas = hour * 60 * 60 + minutes * 60 + seconds;
     }
 
-    private void clockUpdate() {
-        if(doTimeLead){
+    private void clockUpdate()
+    {
+        if (doTimeLead)
+        {
             //月の計算を詰める必要あり。
-            int[] tmp=new int[5]{
+            int[] tmp = new int[]{
                 30*24*60*60*100,
                 24*60*60*100,
                 60*60*100,
@@ -67,37 +72,42 @@ public class EarthRotation : MonoBehaviour {
                 (day - 1) * tmp[1] +
                 hour * tmp[2] +
                 minutes * tmp[3] +
-                seconds*tmp[4] +
-                miliSeconds*tmp[5]
+                seconds * tmp[4] +
+                miliSeconds * tmp[5]
             );
 
-            try {
-                timeWas += Time.deltaTime*tmp[5];
-                
-                month = timeWas/tmp[0]+1;
-                timeWas%=tmp[0];
-                day=timeWas/tmp[1]+1;
-                timeWas%=tmp[1];
-                hour=timeWas/tmp[2];
-                timeWas%=tmp[2];
-                minutes=timeWas/tmp[3];
-                timeWas%=tmp[3];
-                seconds=timeWas/tmp[4];
-                timeWas%=tmp[4];
-                miliSeconds=timeWas/tmp[5];
-                
-                 
-            } catch (System.Exception) {
-                
+            try
+            {
+                timeWas += (int)Time.deltaTime * tmp[5];
+
+                month = timeWas / tmp[0] + 1;
+                timeWas %= tmp[0];
+                day = timeWas / tmp[1] + 1;
+                timeWas %= tmp[1];
+                hour = timeWas / tmp[2];
+                timeWas %= tmp[2];
+                minutes = timeWas / tmp[3];
+                timeWas %= tmp[3];
+                seconds = timeWas / tmp[4];
+                timeWas %= tmp[4];
+                miliSeconds = timeWas / tmp[5];
+
+
+            }
+            catch (System.Exception)
+            {
+
                 throw;
             }
-        }else{
-            month=worldClock.Month;
-            day=worldClock.Day;
+        }
+        else
+        {
+            month = worldClock.Month;
+            day = worldClock.Day;
             hour = worldClock.Hour;
             minutes = worldClock.Minutes;
             seconds = worldClock.Seconds;
-            miliSeconds=worldClock.MiliSeconds;
+            miliSeconds = worldClock.MiliSeconds;
         }
     }
 }
--- Assets/TimeAdministration.cs
+++ Assets/TimeAdministration.cs
@@ -1,7 +1,8 @@
 ﻿using System;
 using UnityEngine;
 //時間を管理
-public class TimeAdministration : MonoBehaviour {
+public class TimeAdministration : MonoBehaviour
+{
     [SerializeField, HeaderAttribute("年")]
     private int year;
     [SerializeField, Range(1, 12), HeaderAttribute("月")]
@@ -14,6 +15,8 @@ public class TimeAdministration : MonoBehaviour {
     private int minutes;
     [SerializeField, Range(0, 59), HeaderAttribute("秒")]
     private int seconds;
+    [SerializeField, Range(0, 59), HeaderAttribute("ミリ秒")]
+    private int miliSeconds;
     [SerializeField, HeaderAttribute("初期値をリアルタイムにする")]
     private bool useRealTime;
     [SerializeField, HeaderAttribute("再生速度。0:静止、1:リアルタイム 2～:x倍速 -:逆再生")]
@@ -28,12 +31,14 @@ public class TimeAdministration : MonoBehaviour {
 
     private readonly int[] month30 = new int[5] { 2, 4, 6, 9, 11 };
 
-    void Start() {
+    void Start()
+    {
         //デバッグ用
         real = 0;
         wasReal = 0;
         //デバッグ用終了
-        if (useRealTime) {
+        if (useRealTime)
+        {
             DateTime now = DateTime.Now;
             year = now.Year;
             month = now.Month;
@@ -41,14 +46,17 @@ public class TimeAdministration : MonoBehaviour {
             hour = now.Hour;
             minutes = now.Minute;
             seconds = now.Second;
+            miliSeconds = now.Millisecond;
         }
     }
 
     //0.02秒ごとに呼ばれる
-    private void FixedUpdate() {
+    private void FixedUpdate()
+    {
         if (simulateSpeed != 0)
             flame++;
-        else {
+        else
+        {
             flame = 0;
             //デバッグ用
             real = Time.realtimeSinceStartup;
@@ -56,8 +64,9 @@ public class TimeAdministration : MonoBehaviour {
             //デバッグ用終了
         }
 
-        if (Mathf.Abs(flame * simulateSpeed) >= fps) {
-            seconds += (int)( ( flame * simulateSpeed ) / (float) fps );
+        if (Mathf.Abs(flame * simulateSpeed) >= fps)
+        {
+            seconds += (int)((flame * simulateSpeed) / (float)fps);
             //デバッグ用
             real = Time.realtimeSinceStartup;
             //Debug.Log((int)( ( flame * simulateSpeed ) / (float)fps ) + "　経過時間　" + ( real - wasReal ));
@@ -65,71 +74,92 @@ public class TimeAdministration : MonoBehaviour {
             //デバッグ用終了
             bool flg;
             //ループチェック実行　ついでにループが終わるまで実行
-            do {
+            do
+            {
                 flg = loopCheck();
             } while (flg);
             flame = 0;
         }
     }
 
-    void dayCheck() {
-        for (int i = 0; i < month30.Length; i++) {
-            if (month == month30[i]) {
-                if (month == 2) {
+    void dayCheck()
+    {
+        for (int i = 0; i < month30.Length; i++)
+        {
+            if (month == month30[i])
+            {
+                if (month == 2)
+                {
                     if (day > 28)
                         day = 28;
-                } else {
+                }
+                else
+                {
                     month = 30;
                 }
             }
         }
     }
 
-    bool loopCheck() {
+    bool loopCheck()
+    {
         //実行チェック
         bool wasLoop = false;
 
         //以下時間が増えた場合
-        if (seconds >= 60) {
+        if (seconds >= 60)
+        {
             seconds -= 60;
             minutes++;
             wasLoop = true;
         }
 
-        if (minutes >= 60) {
+        if (minutes >= 60)
+        {
             minutes -= 60;
             hour++;
             wasLoop = true;
         }
 
-        if (hour >= 24) {
+        if (hour >= 24)
+        {
             hour -= 24;
             day++;
             wasLoop = true;
         }
 
-        for (int i = 0; i < month30.Length; i++) {
+        for (int i = 0; i < month30.Length; i++)
+        {
             //30日の月である
-            if (month == month30[i]) {
-                if (month == 2 && day > 28) {
+            if (month == month30[i])
+            {
+                if (month == 2 && day > 28)
+                {
                     //2月かつ29日以上
                     day -= 28;
                     month++;
                     wasLoop = true;
-                } else if (/* ( year % 4 == 0 && month == 2 && day > 29 )*/false) {
+                }
+                else if (/* ( year % 4 == 0 && month == 2 && day > 29 )*/false)
+                {
                     //閏年の2月30日以上
                     day -= 29;
                     month++;
                     wasLoop = true;
-                } else if (day > 30) {
+                }
+                else if (day > 30)
+                {
                     //30の月かつ31日以上
                     day -= 30;
                     month++;
                     wasLoop = true;
                 }
-            } else if (i >= month30.Length - 1) {
+            }
+            else if (i >= month30.Length - 1)
+            {
                 //最後の一周まで処理されない
-                if (day > 31) {
+                if (day > 31)
+                {
                     day -= 31;
                     month++;
                     wasLoop = true;
@@ -137,53 +167,67 @@ public class TimeAdministration : MonoBehaviour {
             }
         }
 
-        if (month > 12) {
+        if (month > 12)
+        {
             month -= 12;
             year++;
             wasLoop = true;
         }
 
         //以下時間が減った場合
-        if (seconds < 0) {
+        if (seconds < 0)
+        {
             seconds += 60;
             minutes--;
             wasLoop = true;
         }
 
-        if (minutes < 0) {
+        if (minutes < 0)
+        {
             minutes += 60;
             hour--;
             wasLoop = true;
         }
 
-        if (hour < 0) {
+        if (hour < 0)
+        {
             hour += 24;
             day--;
             wasLoop = true;
         }
 
-        for (int i = 0; i < month30.Length; i++) {
+        for (int i = 0; i < month30.Length; i++)
+        {
             //30日の月の次月である
-            if (month == month30[i] + 1) {
-                if (month == 2 + 1 && day < 1) {
+            if (month == month30[i] + 1)
+            {
+                if (month == 2 + 1 && day < 1)
+                {
                     //2月
                     day += 28;
                     month--;
                     wasLoop = true;
-                } else if (/* ( year % 4 == 0 && month == 2 + 1 && day < 1 )*/false) {
+                }
+                else if (/* ( year % 4 == 0 && month == 2 + 1 && day < 1 )*/false)
+                {
                     //閏年の2月
                     day += 29;
                     month--;
                     wasLoop = true;
-                } else if (day < 1) {
+                }
+                else if (day < 1)
+                {
                     //30の月次月
                     day += 30;
                     month--;
                     wasLoop = true;
                 }
-            } else if (i >= month30.Length - 1) {
+            }
+            else if (i >= month30.Length - 1)
+            {
                 //最後の一周まで処理されない
-                if (day < 1) {
+                if (day < 1)
+                {
                     day += 31;
                     month--;
                     wasLoop = true;
@@ -191,7 +235,8 @@ public class TimeAdministration : MonoBehaviour {
             }
         }
 
-        if (month < 1) {
+        if (month < 1)
+        {
             month += 12;
             year--;
             wasLoop = true;
@@ -201,37 +246,50 @@ public class TimeAdministration : MonoBehaviour {
     }
 
     //以下ゲッターとセッター
-    public int Year {
+    public int Year
+    {
         set { this.year = value; }
         get { return this.year; }
     }
 
-    public int Month {
+    public int Month
+    {
         set { this.month = value; }
         get { return this.month; }
     }
 
-    public int Day {
+    public int Day
+    {
         set { this.day = value; }
         get { return this.day; }
     }
 
-    public int Hour {
+    public int Hour
+    {
         set { this.hour = value; }
         get { return this.hour; }
     }
 
-    public int Minutes {
+    public int Minutes
+    {
         set { this.minutes = value; }
         get { return this.minutes; }
     }
 
-    public int Seconds {
+    public int Seconds
+    {
         set { this.seconds = value; }
         get { return this.seconds; }
     }
 
-    public float SimulateSpeed {
+    public int MiliSeconds
+    {
+        set { this.miliSeconds = value; }
+        get { return this.MiliSeconds; }
+    }
+
+    public float SimulateSpeed
+    {
         set { this.simulateSpeed = value; }
         get { return this.simulateSpeed; }
     }
